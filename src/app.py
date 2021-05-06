b3873eee7945f7149a6a8ef7fdff877671b284e4
bungaepyo <bungaepyo@gmail.com>
2021-05-06 13:29:08 -0400

    routes and .gitignore

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 import json
 import os
 from flask import request, Flask
+from datetime import datetime
+from db import Journal, db
 
 app = Flask(__name__)
 db_filename = "journal.db"
@@ -13,8 +15,46 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
-# Routes
+def success_response(data, code=200):
+    return json.dumps({"success": True, "data": data}), code
 
+def failure_response(message, code=404):
+    return json.dumps({"success": False, "error": message}), code
+
+# 1: Get all journals
+@app.route("/")
+@app.route("/journals/")
+def get_all_journals():
+    return success_response([j.serialize() for j in Journal.query.all()])
+
+# 2: Get journal by ID
+@app.route("/journals/<int:journal_id>/")
+def get_journal_by_id(journal_id):
+    journal = Journal.query.filter_by(id = journal_id).first()
+    if journal is None:
+        return failure_response("Journal not found")
+    return success_response(journal.serialize())
+
+# 3: Create a journal
+@app.route("/journals/", methods=["POST"])
+def create_journal():
+    body = json.loads(request.data)
+    title = body.get("title")
+    if title is None:
+        return failure_response("No title provided")
+    description = body.get("description")
+    if description is None:
+        return failure_response("No description provided")
+    date = datetime.now()
+    new_journal = Journal(title = title, description = description, date = date)
+    db.session.add(new_journal)
+    db.session.commit()
+    return success_response(new_journal.serialize(), 201)
+
+# 4: Delete a journal
+
+
+# 4: Update a journal
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
--- src/db.py
+++ src/db.py
@@ -6,18 +6,18 @@ class Journal(db.Model):
     ___tablename___ = "journal"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
-    body = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
     date = db.Column(db.String, nullable=False)
 
     def ___init___(self, **kwargs):
-        self.code = kwargs.get("title")
-        self.name = kwargs.get("body")
+        self.title = kwargs.get("title")
+        self.description = kwargs.get("description")
         self.date = kwargs.get("date")
 
     def serialize(self):
         return {
             "id": self.id,
             "title": self.title,
-            "body": self.body,
+            "description": self.description,
             "date": self.date
         }

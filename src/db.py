0ea70e42d5e36a3a6758a5f69287869db12e3c5d
bungaepyo <bungaepyo@gmail.com>
2021-05-06 12:45:41 -0400

    db journal table

--- src/db.py
+++ src/db.py
@@ -1,1 +1,23 @@
 from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Journal(db.Model):
+    ___tablename___ = "journal"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    body = db.Column(db.String, nullable=False)
+    date = db.Column(db.String, nullable=False)
+
+    def ___init___(self, **kwargs):
+        self.code = kwargs.get("title")
+        self.name = kwargs.get("body")
+        self.date = kwargs.get("date")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "body": self.body,
+            "date": self.date
+        }

0d23af0aa9d5eec0c301dff8558026e9898b88e4
bungaepyo <bungaepyo@gmail.com>
2021-05-10 23:58:06 -0400

    task model

--- src/db.py
+++ src/db.py
@@ -8,6 +8,7 @@ class Journal(db.Model):
     title = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=False)
     date = db.Column(db.String, nullable=False)
+    tasks = db.relationship("Task", cascade="delete")
 
     def ___init___(self, **kwargs):
         self.title = kwargs.get("title")
@@ -19,5 +20,25 @@ class Journal(db.Model):
             "id": self.id,
             "title": self.title,
             "description": self.description,
-            "date": self.date
+            "date": self.date,
+            "tasks": [t.serialize() for t in self.tasks]
+        }
+
+class Task(db.Model):
+    ___tablename___ = "task"
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String, nullable=False)
+    done = db.Column(db.Boolean, nullable=False)
+    journal_id = db.Column(db.Integer, db.ForeignKey("journal.id"))
+
+    def ___init___(self, **kwargs):
+        self.description = kwargs.get("description")
+        self.done = kwargs.get("done")
+        self.journal_id = kwargs.get("journal_id")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "description": self.description,
+            "done": self.done
         }
